c52f342effdd4756633ed7f2e24d746dc1fe05d0
bsavyg <bsaveranceg@gmail.com>
2019-12-16 15:47:15 -0500

    scripts.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!!!!');
+var firstName = 'Blake';
+console.log(firstName);
+
+var lastName = 'Grant';
+var age = 28;
+
+console.log(firstName, lastName, age);
+
+var fullAge = true; 
+console.log(fullAge);
+
+var job; 
+console.log(job)
