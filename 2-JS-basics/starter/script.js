dca36ab033bd5ca949019e41c210083c3a3949a9
bsavyg <bsaveranceg@gmail.com>
2020-01-02 15:45:05 -0500

    operator precendence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -29,9 +29,8 @@ var if = 30;
 
 /**************************************
 * Variable mutation and type coercion
-*
-*/
 
+/*
 var firstName = 'Blake';
 var age = 30;
 
@@ -52,3 +51,65 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+*/
+ 
+/***********************
+ *  Basic Operators 
+ 
+
+ /*
+ var year, yearBlake, yearDevin;
+ year = 2020;
+ ageBlake = 30; 
+ ageDevin = 29; 
+
+ //Math Operators 
+ yearBlake = year - 30; 
+ yearDevin = year - 29; 
+
+ console.log(yearBlake, yearDevin);
+ console.log(year + 2);
+ console.log(year * 2);
+ console.log(year / 10);
+
+ // Logical Operators 
+var blakeOlder = ageBlake < ageDevin;
+console.log(blakeOlder);
+
+// Typeof Operators 
+console.log(typeof blakeOlder);
+console.log(typeof yearBlake);
+console.log(typeof 'Blake is older than Devin');
+var x;
+console.log(typeof x);
+
+/******************
+ * Operator precedence
+
+
+ var year = 2020; 
+ var yearBlake = 1989; 
+ var fullAge = 30; 
+
+ // Multiple Operators
+ var isFullAge = year - yearBlake >= fullAge; // true 
+ console.log(isFullAge);
+
+ //Grouping 
+ var ageBlake = year - yearBlake;
+ var ageDevin = 29;
+ var average = (ageBlake + ageDevin) / 2;
+ console.log(average);
+
+ */
+
+ // Multiple Assignments 
+ var x, y; 
+ x = y = (3 + 5) * 4 - 6; // 8 * 4 -6 // 32 - 6 // 26
+ console.log(x, y);
+
+ // More Operators 
+ x *= 2; 
+ console.log(x);
+ x += 10; 
+ console.log(x);
